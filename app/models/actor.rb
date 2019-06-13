fb4b63ab98ab45d5c65f53cb1d338edd0e6d9017
Malcolm Leake <echelon1089@gmail.com>
2019-06-13 22:21:58 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name()
+  "#{self.first_name} #{self.last_name}" 
+  #binding.pry
+  end
+  
+  def list_roles
+    self.characters.collect do |t|
+    "#{t.name} - #{t.show.name}"
+    end
+  end 
+  
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
   
+  def say_that_thing_you_say
+   "Steve Urkel always says: #{self.catchphrase}"
+    #binding.pry
+  end
+  
+  def build_show
+  
+  end 
 end
